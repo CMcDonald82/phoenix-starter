9b06e6a5168273d70c8509a34080c38cc4a4d01b
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-02-08 15:25:15 -0500

    configure DB settings in test config to use environment variables

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :phoenix_starter, PhoenixStarter.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
   database: "phoenix_starter_test",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOSTNAME"),
   pool: Ecto.Adapters.SQL.Sandbox

b4902c3b756e9c4a6aaa1c7c884cbe1a36fd39a7
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-02-08 14:49:15 -0500

    add app.js file

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// Import dependencies
+//
+// If you no longer want to use a dependency, remember
+// to also remove its path from "config.paths.watched".
+// import "phoenix_html"
+
+// Import local files
+//
+// Local files can be imported directly using relative
+// paths "./socket" or full ones "web/static/js/socket".
+
+// import socket from "./socket"
+
+import '../css/app.scss'
+
+console.log("HI!!");

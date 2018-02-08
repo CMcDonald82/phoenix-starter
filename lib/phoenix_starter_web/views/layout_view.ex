0c57f33821d4846e365d6d49d0394c2d78c1dcc2
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-02-08 15:16:43 -0500

    add js_script_tag and css_link_tag to layout view

--- lib/phoenix_starter_web/views/layout_view.ex
+++ lib/phoenix_starter_web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule PhoenixStarterWeb.LayoutView do
   use PhoenixStarterWeb, :view
+
+  def js_script_tag do
+    if Application.get_env(:phoenix_starter, :environment) == :prod do
+      ~s(<script src="/js/app.js"></script>)
+    else
+      ~s(<script src="http://0.0.0.0:8080/js/app.js"></script>)
+    end 
+  end
+
+  def css_link_tag do
+    if Application.get_env(:phoenix_starter, :environment) == :prod do
+      ~s(<link rel="stylesheet" type="text/css" href="/css/app.css" media="screen,projection" />)
+    else
+      ""
+    end
+  end
+
 end

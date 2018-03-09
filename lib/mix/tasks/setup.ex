533d51f61c45a52810d474c66fabee64712510a6
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-03-09 17:14:46 -0500

    add more functions to mix setup task

--- lib/mix/tasks/setup.ex
+++ lib/mix/tasks/setup.ex
@@ -4,18 +4,14 @@ defmodule Mix.Tasks.Setup do
   @shortdoc "Sets up a new clean-slate project using the phoenix-starter project as a base template"
 
   @doc """
-  This function runs several other functions: 
-    - rename_app(name, otp_name): rename the app (using the provided 'name' and 'otp_name' params)
-    - remove_rename_dependency(): Remove the rename module (since it will no longer be needed in the new project being created)
-    - Remove the mix task (this module)
-    - Remove
+  The main function that runs all the necessary functions to setup the new app from the phoenix-starter project  
 
   # NOTE: Should we run yarn install in this list of tasks? or have users do it separately from the command line as a docker-compose command (note that users will be instructed to run the mix setup task within a Docker container)
   
   """
   def run([name, otp_name]) do
     with :ok <- rename_app(name, otp_name),
-         :ok <- remove_rename_dependency(),
+         :ok <- remove_rename_dep(),
          # :ok <- yarn_init() # Run yarn install here?
          :ok <- remove_mix_task(),
          :ok <- remove_setup_config(),
@@ -62,4 +58,63 @@ defmodule Mix.Tasks.Setup do
   def config do
     Application.get_env(:phoenix_starter, Mix.Tasks.Setup)
   end
+
+  @doc """
+  Renames the app using either the 'name' and 'otp_name' params provided on the command line or the 'name' 
+  and 'otp_name' params provided in the setup config at config/setup.exs. 
+  """
+  def rename_app(name, otp_name) do
+    Mix.Shell.IO.info "Renaming app to #{name}/#{otp_name}"
+    Rename.run(
+      {"PhoenixStarter", name},
+      {"phoenix_starter", otp_name}
+    )
+    :ok
+  rescue 
+    _ -> {:error, "Failed to rename the app to #{name}/#{otp_name}"}
+  end
+
+  @doc """
+  Removes the Rename dependency from deps in mix.exs (since it will no longer be needed in the new 
+  project being created)
+  """
+  defp remove_rename_dep do
+    Mix.Shell.IO.info "Removing rename dependency"
+    with_rename_dep_removed = "mix.exs"
+    |> File.read!
+    |> String.split("\n")
+    |> Enum.reject(&(&1 |> String.contains?(":rename")))
+    |> Enum.join("\n")
+    File.write!("mix.exs", with_rename_dep_removed)
+  end
+
+  @doc """
+  Removes this task (the mix task) since we don't need it anymore once the new project is configured
+  """
+  defp remove_mix_task do
+    Mix.Shell.IO.info "Removing this mix task (since it is no longer needed once the new project has been setup)"
+    Mix.Shell.IO.cmd("rm -rf lib/mix/tasks/setup.ex")
+    :ok
+  rescue
+    _ -> {:error, "Failed to remove mix task"}
+  end
+
+  @doc """
+  Removes the setup config file since we don't need it anymore once the new project is configured
+  The setup config file is imported in config/dev.exs
+  """
+  defp remove_setup_config do
+    Mix.Shell.IO.info "Removing setup config file"
+    Mix.Shell.IO.cmd("rm -rf config/setup.exs")
+    with_import_config_removed = "config/dev.exs"
+    |> File.read!
+    |> String.split("\n")
+    |> Enum.reject(&(&1 |> String.contains?("setup.exs")))
+    |> Enum.join("\n")
+    File.write!("config/dev.exs", with_import_config_removed)
+  rescue
+    _ -> {:error, "Failed to remove setup config import"}
+  end
+
+  
 end

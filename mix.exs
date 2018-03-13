e4281a0d9889e49aa15117f4e4a8e1aa7b469e1b
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-03-13 17:04:15 -0400

    remove the only: :dev option in rename module in mix.exs since the rename module will be used in both dev and test

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule PhoenixStarter.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:rename, "~> 0.1.0", only: :dev},
+      {:rename, "~> 0.1.0"},
       {:edeliver, "~> 1.4.5"},
       {:distillery, "~> 1.5"}
     ]

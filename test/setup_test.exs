ef1110876c7a194e662bd8c71b6679abd41c6783
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-03-14 10:44:36 -0400

    changed test for setup task to check the mix.exs file that the app has been renamed

--- test/setup_test.exs
+++ test/setup_test.exs
@@ -5,8 +5,10 @@ defmodule SetupTest do
   @app_name "PhoenixStarterTestApp"
   @git_repo "https://github.com/CMcDonald82/phoenix-starter.git"
 
+
+  # NOTE: Instead of curl'ing localhost (which doesn't seem to work in a docker container in travis test), why don't we just test that the layout template (lib/phoenix_starter_web/templates/layout contains Hello #{@app_name}!
   @tag timeout: :infinity
-  test "sets up app with default config" do
+  test "sets up app with passed in app name params" do
     File.cd!("..")
     :os.cmd('rm -rf #{@app_dir}')
     git_clone_starter()
@@ -14,37 +16,43 @@ defmodule SetupTest do
     :os.cmd('mix deps.get')
     # :os.cmd('mix compile')
     :os.cmd('mix setup #{@app_name} #{@app_dir}')
-    start_server()
-    :timer.sleep(10000)
-    :os.cmd('curl http://localhost:4000')
+    assert check_app_renamed(name)
+    # start_server()
+    # :timer.sleep(10000)
+    # :os.cmd('curl http://localhost:4000')
     # {page, 0} = System.cmd("curl", ["localhost:4000"])
     # assert page |> String.contains?("Hello #{@app_name}!")
-    kill_server()
+    # kill_server()
   end
 
   defp git_clone_starter do
     :os.cmd('git clone #{@git_repo} #{@app_dir}')
   end
 
-  defp start_server do
-    spawn fn ->
-      :os.cmd('mix phx.server')
-    end
+  defp check_app_renamed(name) do
+    renamed = File.read!("mix.exs")
+    |> Enum.member?(@app_name)
   end
 
-  defp kill_server do
-    "ps"
-    |> System.cmd(["-ef"])
-    |> elem(0)
-    |> String.split("\n")
-    |> Enum.filter(&(&1 |> String.contains?("mix phx.server")))
-    |> Enum.each(fn process ->
-      pid = process
-      |> String.split(" ")
-      |> Enum.reject(&(&1 == ""))
-      |> Enum.at(1)
-      :os.cmd('kill -9 #{pid}')
-    end)
-  end
+  # defp start_server do
+  #   spawn fn ->
+  #     :os.cmd('mix phx.server')
+  #   end
+  # end
+
+  # defp kill_server do
+  #   "ps"
+  #   |> System.cmd(["-ef"])
+  #   |> elem(0)
+  #   |> String.split("\n")
+  #   |> Enum.filter(&(&1 |> String.contains?("mix phx.server")))
+  #   |> Enum.each(fn process ->
+  #     pid = process
+  #     |> String.split(" ")
+  #     |> Enum.reject(&(&1 == ""))
+  #     |> Enum.at(1)
+  #     :os.cmd('kill -9 #{pid}')
+  #   end)
+  # end
 
 end

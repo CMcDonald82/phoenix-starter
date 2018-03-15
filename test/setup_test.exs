52e6e440c3a1a6bc5f5f0a494d8306902a17ad09
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-03-15 13:47:34 -0400

    add page variable

--- test/setup_test.exs
+++ test/setup_test.exs
@@ -36,8 +36,8 @@ defmodule SetupTest do
     # NOTE: Not needed anymore
     start_server()
     :timer.sleep(10000)
-    :os.cmd('curl http://$(docker-machine ip default):4000')
-    
+    page = :os.cmd('curl http://$(docker-machine ip default):4000')
+
     # {page, 0} = System.cmd("curl", ['$(docker-machine ip default):4000'])
     # {page, 0} = System.cmd("curl", ["localhost:4000"])
     assert page |> String.contains?("Hello #{@app_name}!")
